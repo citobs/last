107031fd5917fd87d43c5774c1f295ff367e8639
citobs <altiorapetamus7@hanmail.net>
2024-08-01 23:08:35 +0900

    포트수정

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -4,6 +4,7 @@ module.exports = {
         staticDistDir: "./build",
         //startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
         url: ["https://www.amoremall.com:443/kr/ko/display/main"],
+        port:443,
         numberOfRuns: 5,
       },
       upload: {
