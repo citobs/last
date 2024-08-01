395445e7631664020f22cfc1c023632761c2706d
citobs <altiorapetamus7@hanmail.net>
2024-08-01 23:29:15 +0900

    ok

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -3,13 +3,15 @@ module.exports = {
       collect: {
         staticDistDir: "./build",
         //startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
-        url: ["https://www.amoremall.com:443/kr/ko/display/main"],
-        port:443,
+        url: ["https://www.amoremall.com:443/kr/ko/display/main"],       
         numberOfRuns: 5,
       },
       upload: {
         target: "temporary-public-storage",
       },
+      server:{
+        port:443
+      }
     },
   };
 
