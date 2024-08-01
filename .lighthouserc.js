e20da4c95e5042ad243dc9471ea0b13806e195dc
citobs <altiorapetamus7@hanmail.net>
2024-08-01 23:00:45 +0900

    포트수정

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -3,7 +3,7 @@ module.exports = {
       collect: {
         staticDistDir: "./build",
         //startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
-        url: ["https://www.amoremall.com/kr/ko/display/main"],
+        url: ["https://www.amoremall.com:443/kr/ko/display/main"],
         numberOfRuns: 5,
       },
       upload: {
